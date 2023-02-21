243a538200ecc452126ab51b1965fca96a938486
Artem Trusov <tavikingwork@gmail.com>
2023-02-21 17:59:26 +0200

    added Js & fixed styles + markup

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
-@import 'common.scss';
+const getTimeDifference = () => {
+  const [h1, m1] = document
+    .getElementById('startTime')
+    .value.split(':')
+    .map(Number);
+  const [h2, m2] = document
+    .getElementById('endTime')
+    .value.split(':')
+    .map(Number);
+  const diff = Math.abs(h2 * 60 + m2 - (h1 * 60 + m1));
+  const [hours, minutes] = [Math.floor(diff / 60), diff % 60];
+  const fractionOfHour = minutes / 60;
+
+  const formattedHours = hours < 10 ? `0${hours}` : hours;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedFractionOfHour = (hours + fractionOfHour).toFixed(2);
+
+  return `${formattedHours}:${formattedMinutes} (${formattedFractionOfHour})`;
+};
+
+document
+  .querySelector('.form__btn')
+  .addEventListener(
+    'click',
+    () =>
+      (document.querySelector('.result__item').innerHTML = getTimeDifference())
+  );
